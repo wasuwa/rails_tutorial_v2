32ce1ffcbb3f3494259117b4bfe9c1f515ad0619
suwa <baru.web.production@gmail.com>
2021-06-28 20:06:53 +0900

    config.generatorsを追加し、テストが作成されないようにする

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module RailsTutorialV2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework false
+    end
   end
 end
