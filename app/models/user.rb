550d8bd1a0204310d7f2c6e25e699863bf8b3fb3
suwa <baru.web.production@gmail.com>
2021-07-04 15:28:55 +0900

    rememberメソッドを作成する#

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
   validates :name,
     presence: true,
@@ -13,4 +15,22 @@ class User < ApplicationRecord
     presence: true,
     length: { minimum: 6 }
   has_secure_password
+
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def .new_token
+      SecureRandom.urlsafe_base64
+    end
+
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 end
