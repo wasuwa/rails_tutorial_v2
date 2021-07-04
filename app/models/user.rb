42f4f147af5172347f51ea073511162cee53b6d8
suwa <baru.web.production@gmail.com>
2021-07-04 15:42:33 +0900

    cookieを作成する

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
       BCrypt::Password.create(string, cost: cost)
     end
 
-    def .new_token
+    def new_token
       SecureRandom.urlsafe_base64
     end
 
@@ -32,5 +32,9 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
