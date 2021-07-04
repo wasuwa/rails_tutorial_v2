727302b4b2e53577532c811b960684af454b8d36
suwa <baru.web.production@gmail.com>
2021-07-04 21:07:05 +0900

    画像投稿機能を完成させる

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   default_scope -> { order(created_at: :desc) }
 
@@ -8,4 +9,12 @@ class Micropost < ApplicationRecord
   validates :content,
     presence: true,
     length: { maximum: 140 }
+  validates :image,
+    content_type: { in: %w[image/jpeg image/gif image/png],
+    message: "must be a valid image format" },
+    size:    { less_than: 5.megabytes, message: "should be less than 5MB" }
+
+  def display_image
+    image.variant(resize_to_limit: [500, 500])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,10 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
     def downcase_email
